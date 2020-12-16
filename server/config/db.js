085d58bf7d41185368a88316c5222cb6dca4b12d
Seth Brown <srbrown11@mail.fhsu.edu>
2020-12-15 23:37:51 -0600

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://app:I2finit*1@localhost:27017/app',
+  dbOptions: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+};
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/db');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to database
+mongoose
+  .connect(config.mongoURI, config.dbOptions)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Start server
+app.listen(port, () => console.log(`Server started on port ${port}`));
